6d2274514809282265736cde46800b52c249db30
Certil Remy <certil@noukod.com>
2018-10-15 15:51:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
-class Song 
-  
+class Song
+
   attr_accessor :name,:artist
-  
+
   @@all=[]
-  
+
   def initialize(name)
     @name=name
     @@all << self
   end
-  
+
   def artist_name
     if(@artist)
       @artist.name
-    else 
+    else
       nil
     end
   end
-  
+
 end

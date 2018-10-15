2d055533aebc46c894d67f336aa73b716749129c
Certil Remy <certil@noukod.com>
2018-10-15 15:49:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,29 @@
-class Artist 
-  
+class Artist
+
   attr_accessor :name,:songs
-  
+
   @@songs=[]
-  
+
   def initialize(name)
     @name=name
     @songs=[]
   end
-  
+
   def add_song(song)
-    @songs << song 
+    @songs << song
     song.artist=self
-    @@songs << song 
+    @@songs << song
   end
-  
+
   def add_song_by_name(name)
    song = Song.new(name)
    songs << song
    @@songs << song
    song.artist = self
   end
-  
-  def self.song_count 
+
+  def self.song_count
     @@songs.count
   end
-  
+
 end
